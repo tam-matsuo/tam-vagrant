d4f9016a524fa0e30b83dc62cd1f09355df0cf61
TAM_matsuo <matsuo@tam-tam.co.jp>
2014-06-27 11:05:14 +0900

    index, phpinfo できた

--- sites/index.php
+++ sites/index.php
@@ -1,4 +1,41 @@
 <?php
-echo $_SERVER['HTTP_HOST'];
-echo '<hr />';
-phpinfo();
+$ret = shell_exec('ifconfig | grep 192.');
+$ip_ad = preg_replace('/^.*addr:(192\S*) .*$/', '$1', trim($ret));
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>tamvagrant</title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<h1><?= $_SERVER['HTTP_HOST'] ?></h1>
+
+<hr/>
+
+<ul>
+<?php
+foreach (glob('*') as $filename) {
+	if (! is_dir($filename)) {
+		continue;
+	}
+
+	$url = "http://{$filename}.{$ip_ad}.xip.io";
+	echo '<li><a href="' . $url . '">' . $filename . '</a></li>';
+}
+?>
+</ul>
+
+<hr/>
+
+<ul>
+	<li><a href="#">phpMyAdmin</a></li>
+	<li><a href="info.php">phpinfo()</a></li>
+</ul>
+
+</div>
+</body>
+</html>
---
+++ sites/info.php
@@ -0,0 +1,2 @@
+<p><a href="/"><?= $_SERVER['HTTP_HOST'] ?></a></p>
+<?php phpinfo();
